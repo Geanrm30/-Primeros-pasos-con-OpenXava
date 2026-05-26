5f3d82506d8b5d5903196d86c28598f11e5739b6
Geanrm30 <garodriguezm@uamv.edu.ni>
2026-05-26 10:49:46 -0600

    Leccion 5 completa

--- src/main/java/ni/edu/uam/facturacion/modelo/Autor.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Autor.java
@@ -5,6 +5,8 @@ import org.hibernate.annotations.GenericGenerator;
 import org.openxava.annotations.*;
 import lombok.*;
 
+import java.util.Collection;
+
 @Entity @Getter @Setter
 public class Autor {
 
@@ -16,4 +18,7 @@ public class Autor {
     @Column(length=50) @Required
     String nombre;
 
+    @OneToMany(mappedBy="autor")
+    @ListProperties("numero, descripcion, precio")
+    Collection<Producto> productos;
 }
